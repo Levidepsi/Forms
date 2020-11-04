b13ba9ad35f0b3078eab2688a304322577e587c9
Levidepsi <levidepsi7@gmail.com>
2020-11-04 14:55:04 +0800

    forms

---
+++ app.js
@@ -0,0 +1,65 @@
+    // var arr = [
+    //     [1,2], [3,4], [5,6]
+    // ];
+    // for (var i=0; i < arr.length; i++) {
+    //     for (var j=0; j < arr[i].length; j++) {
+    //     console.log(arr[i][j]);
+    //     }
+    // }
+    // console.log(arr);
+
+    // function multiplyAll(arr) {
+    //     var product = 1;
+    //     // Only change code below this line
+    //     for(var i=0; i < arr.length; i++) {
+    //         for(var j=0; j < arr[i].length; j++){
+    //             product *= arr[i][j];
+    //         }
+    //     }
+    //     console.log(product);
+    //     // Only change code above this line
+    //     return product;
+    //   }
+    //   multiplyAll([[1,2],[3,4],[5,6,7]]);
+      
+    
+    // function multiplyAll(arr) {
+    //     var product = 1;
+        
+    //     for (var i=0; i < arr.length; i++) {
+    //       for (var j=0; j < arr[i].length; j++) {
+    //         product *= arr[i][j];
+    //       }
+    //     }
+      
+    //     return product;
+    //   }
+      
+    //   var product = multiplyAll([[1,2],[3,4],[5,6,7]]);
+      
+    //   console.log(product);
+      
+    // function multiply(arr, n) {
+    //     var product = 1;
+    //     for (var i = 0; i < n; i++) {
+    //         product *= arr[i];
+           
+    //     }
+    //     console.log(arr, an);
+    //     return product;
+    //   }
+
+    // Only change code below this line
+// function countdown(n){
+//     if (n < 1) {
+//         return [];
+//     }else {
+//         const arr = countdown(n - 1);
+//         arr.unshift(n)
+//         return arr;
+//     }
+  
+//   }
+//   console.log(countdown(n));
+//   // Only change code above this line
+  
