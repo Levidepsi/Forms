42c8b1fab092927ca2d5f995fb9453ce7178e928
Levidepsi <69233374+Levidepsi@users.noreply.github.com>
2021-01-21 08:21:19 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const dotenv = require('dotenv')
+
+
+dotenv.config({path: './config/config.env'})
+
+const app = express();
+
+const PORT = process.env.PORT || 9000;
+
+
+app.listen(
+    PORT, console.log(`Serving is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+
+// const todos = [
+//     {id: 1, text: 'Todo one'},
+//     {id: 2, text: 'Todo two'},
+//     {id: 3, text: 'Todo three'}
+// ]
+
+// const server = http.createServer ((req, res) => {
+//     // res.setHeader('Content-Type', 'text/plain')
+//     // res.setHeader('Content-Type', 'application/json')
+//     // res.setHeader('X-Powered-By', 'Node.js')
+//     const {method, url} = req;
+//     let body = [];
+//     req.on('data', chunk => {
+//         body.push(chunk);
+//     }).on('end', () => {
+//         body = Buffer.concat(body).toString()
+        
+//         let status = 404;
+//         const response = {
+//             success: false,
+//             data: null
+//         }
+
+//         if (method === 'GET' && url === '/todos') {
+//             status = 200;
+//             response.success = true;
+//             response.data = todos;
+//         } else if(method === 'POST' && url === '/todos') {
+//             const { id, text} = JSON.parse(body)
+//             todos.push({ id, text});
+//             status = 200;
+//             response.success = true;
+//             response.data = todos;
+
+//             if(!id || !text) {
+//                 status = 400;
+//                 response.error = 'please add an id and text'
+//             } else {
+//                 status = 200;
+//                 response.success = true;
+//                 response.data = todos;
+//             }
+            
+//         }
+//         res.writeHead(status, {
+//             'Content-Type': 'application/json',
+//             'X-Powered-By': 'Node.js'
+//         });
+//         console.log(req.headers.authorization)
+//         res.end(JSON.stringify(response)
+//         );
+//     });
+
+// });
+
+// PORT =  10000;
+
+// server.listen(PORT, () => console.log(`server is running on port ${PORT}` ));
